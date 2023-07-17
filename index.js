909f8b786ef1e05a95c096ef7a72981a04945bb1
sabbir <sabbir@gmail.com>
2023-07-17 14:46:04 +0600

    database connect

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
-app.listen(process.env.PORT, () => console.log("code is running"));
+const client = new MongoClient(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    app.listen(process.env.PORT, () => console.log("code is running"));
+  } catch (err) {
+    console.log("database connection failed", err);
+  }
+}
+
+run();
